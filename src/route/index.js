a601c49e75d18d2c886e2f17a681d5632c275bd0
Sendi Alamsyah <sendialamsyah02@gmail.com>
2022-07-11 18:49:37 +0700

    first commit

--- src/route/index.js
+++ src/route/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const airlines = require("./airlines");
 
-router.use("/airlines", airlines);
+router
+    .use("/airlines", airlines)
 
 module.exports = router;
