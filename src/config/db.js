843ba0849be92aea544ad30c19629a633bd5be27
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-12 18:42:35 +0700

    using ssl deployment

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,14 @@ const { Pool } = require('pg')
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString : `postgres://pslvlloebbtgtd:ac79a695c160528d63ece956597b2037b68838fef7b8dc0bc85ff5f519ad9a74@ec2-3-217-14-181.compute-1.amazonaws.com:5432/d58t23tntj4edt
+  `,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized : false }
 })
 
 pool.connect((error) => {
