79424e9947ba599e4dfae60d04f428604f1dd303
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-12 18:35:49 +0700

    just edit route

--- src/route/tiketRoute.js
+++ src/route/tiketRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getAllTicket, getTicketbyFilter, addTicket, updateTicket, deleteTicket } = require('../controller/tiketcontroller')
+const { getAllTicket, getTicketbyFilter, addTicket, updateTicket, deleteTicket } = require('../controller/tiketController')
 const router = express.Router()
 
 router
