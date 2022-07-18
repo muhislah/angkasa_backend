8642f5bc2035de4dc3f59bde5f43b9d90f1a5a32
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-18 10:05:25 +0700

    add gate, terminal, airport, CRU

--- src/model/tiketModel.js
+++ src/model/tiketModel.js
@@ -38,19 +38,19 @@ const getAllTicket = ({ transit, facilities, departure, arrive, airline, min_pri
     const vairline = airline ? `and a.airlinename ILIKE '%${airline}%' ` : ''
     const vprice = max_price ? `and t.price BETWEEN '${+min_price}' and '${+max_price}' ` : ''
     const vdestination = destination ? `t.destination ILIKE '%${destination}%' ` : `t.destination ILIKE '%%'`
-    return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive , t.price, to_char(t.departure_date, 'FMDay, DD FMMonth YY') as date, t.origin, t.country_origin,  t.destination, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid where " + vdestination + vcountry_origin + vcountry_destination + vkelas + vtransit + vfacilities + vdeparture + varrive + vairline + vprice + pagination)
+    return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive , t.gate, t.terminal, t.airport, CONCAT(t.gate,'-',t.terminal) as code, t.price, to_char(t.departure_date, 'FMDay, DD FMMonth YY') as date, t.origin, t.country_origin,  t.destination, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid where " + vdestination + vcountry_origin + vcountry_destination + vkelas + vtransit + vfacilities + vdeparture + varrive + vairline + vprice + pagination)
 
   } else {
-    return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive, t.price, to_char(t.departure_date, 'FMDay, DD FMMonth YYYY') as date, t.origin, t.country_origin,  t.destination, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid "+pagination)
+    return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive, t.gate, t.terminal, t.airport, CONCAT(t.gate,'-',t.terminal) as code, t.price, to_char(t.departure_date, 'FMDay, DD FMMonth YYYY') as date, t.origin, t.country_origin,  t.destination, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid "+pagination)
   }
 }
 
 const getTicketDetail = (id) => {
-  return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive, t.price, to_char(t.created_at, 'FMDay, DD FMMonth YYYY') as date, t.origin, t.country_origin,  t.destination, t.airline_id, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid WHERE t.id = $1",[id])
+  return pool.query("SELECT t.id, t.transit, t.facilities, to_char(t.departure , 'HH24:MI') as departure , to_char(t.arrive , 'HH24:MI') as arrive, t.gate, t.terminal, t.airport, CONCAT(t.gate,'-',t.terminal) as code, t.price, to_char(t.departure_date, 'FMDay, DD FMMonth YYYY') as date, t.origin, t.country_origin,  t.destination, t.airline_id, t.country_destination, t.class, a.airlinename as airline , a.logo as airline_logo, t.stock FROM tickets as t JOIN airlines as a ON t.airline_id = a.airlineid WHERE t.id = $1",[id])
 }
 
-const addTicket = ({ id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, class : kelas, stock, departure_date }) => {
-  return pool.query('INSERT INTO tickets (id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, class, stock, departure_date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)', [id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, kelas , stock, departure_date])
+const addTicket = ({ id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, class : kelas, stock, departure_date, gate, terminal, airport }) => {
+  return pool.query('INSERT INTO tickets (id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, class, stock, departure_date, gate, terminal, airport) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)', [id, transit, facilities, departure, arrive, price, airline_id, origin, destination, country_origin, country_destination, kelas , stock, departure_date, gate, terminal, airport])
 }
 
 const countData = ({ transit, facilities, departure, arrive, airline, min_price, max_price, destination, country_origin, country_destination, class : kelas }) => {
@@ -95,7 +95,7 @@ const countData = ({ transit, facilities, departure, arrive, airline, min_price,
 }
 
 
-const updateTicket = ({ id, transit, facilities, departure, departure_date, arrive, price, airline_id, origin, destination, stock, country_origin, country_destination, class : kelas }) => {
+const updateTicket = ({ id, transit, facilities, departure, departure_date, arrive, price, airline_id, origin, destination, stock, country_origin, country_destination, class : kelas , gate, terminal, airport}) => {
   const vtransit = transit || null
   const vfacilities = facilities || null
   const vdeparture = departure || null
@@ -109,7 +109,10 @@ const updateTicket = ({ id, transit, facilities, departure, departure_date, arri
   const vcountry_destination = country_destination || null
   const vkelas = kelas || null
   const vdeparture_date = departure_date || null
-  return pool.query('UPDATE tickets SET transit = COALESCE($1, transit) ,  facilities = COALESCE($2, facilities), departure = COALESCE($3, departure), arrive = COALESCE($4, arrive), price = COALESCE($5, price) , airline_id =  COALESCE($6, airline_id), origin = COALESCE($7, origin), destination = COALESCE($8, destination), stock = COALESCE($9, stock) ,country_origin = COALESCE($10,country_origin), country_destination = COALESCE($11,country_destination), class = COALESCE($12, class), departure_date = COALESCE($13, departure_date)  WHERE id = $14', [vtransit, vfacilities, vdeparture, varrive, vprice, vairline_id, vorigin, vdestination, vstock, vcountry_origin, vcountry_destination, vkelas, vdeparture_date, id])
+  const vgate = gate || null
+  const vterminal = terminal || null
+  const vairport = airport || null
+  return pool.query('UPDATE tickets SET transit = COALESCE($1, transit) ,  facilities = COALESCE($2, facilities), departure = COALESCE($3, departure), arrive = COALESCE($4, arrive), price = COALESCE($5, price) , airline_id =  COALESCE($6, airline_id), origin = COALESCE($7, origin), destination = COALESCE($8, destination), stock = COALESCE($9, stock) ,country_origin = COALESCE($10,country_origin), country_destination = COALESCE($11,country_destination), class = COALESCE($12, class), departure_date = COALESCE($13, departure_date), gate = COALESCE($14, gate), terminal = COALESCE($15, terminal), airport = COALESCE($16, airport) WHERE id = $17', [vtransit, vfacilities, vdeparture, varrive, vprice, vairline_id, vorigin, vdestination, vstock, vcountry_origin, vcountry_destination, vkelas, vdeparture_date, vgate, vterminal, vairport, id])
 }
 
 const deleteTicket = (id) => {
