9e5792370eea95301836c97b0ca301a69fbaa02a
Sendi Alamsyah <sendialamsyah02@gmail.com>
2022-07-18 11:11:23 +0700

    add insurance & detail

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const airlines = require("./airlines");
 const orderedTicket = require('./orderedTicket')
 
 router
-    .use("/airlines", airlines)
     .use("/ordered-ticket", orderedTicket)
 
 module.exports = router;
